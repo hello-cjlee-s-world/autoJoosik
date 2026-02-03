13d92cae9991e3adf1dea316fdc469124c742ec0
hello-cjlee-s-world <cndwn2131@naver.com>
2026-02-03 22:22:39 +0900

    feat: 설정 파일 api 정보 추가

--- src/main/java/org/cjlee/auto/autojoosik/config/CProperties.java
+++ src/main/java/org/cjlee/auto/autojoosik/config/CProperties.java
@@ -8,41 +8,9 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "manager")
 @Data
 public class CProperties {
-	private String tenantCd;
-	private String portalCode;
-	private String nodeId;
-	private String mediaWallId;
-	private String postImageRootPath;
-	private String postImageRootUrl;
-	private String bannerRootPath;
-	private String bannerRootUrl;
-	private String popupRootPath;
-	private String popupRootUrl;
-	private String sourceRootPath;
-	private String attachRootPath;
-	private String fileTempRootPath;
-	private String fileTempRootUrl;
-	private String playerVodRootUrl;
-	private String playerSnakeimageRootUrl;
-	private String liveUrlDefault;
-	private String profileImageRootPath;
-	private String profileImageRootUrl;
 	private Jwt jwt;
-	private KakaoMap kakaoMap;
-
-	private String amqpUri;
-	private String ffmpegFilePath;
-	private String ffprobeFilePath;
-	private String contentRootPath;
-	private String contentRootUrl;
-	private String podImageRootPath;
-	private String podImageRootUrl;
-	private String nodeImageRootPath;
-	private String nodeImageRootUrl;
-	private String defualtPostimagePath;
-	private String defualtPostimageName;
-	private String busshelterOutdoorVideoUrl;
-	private String InfojectorAdminUrl;
+    private Api api;
+    private String fileTempRootPath;
 
 	@Data
 	public static class Jwt {
@@ -52,9 +20,9 @@ public class CProperties {
 		private Integer refreshTimeoutMin;
 		private String secret;
 	}
-
-	@Data
-	public static class KakaoMap {
-		private String appKey;
-	}
+    @Data
+    public static class Api {
+        private String host;
+        private String port;
+    }
 }
